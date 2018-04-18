f69240c100c620b056d22f9978a8b6be06c08a4d
Scott Wu <scott.wu.hjx@gmail.com>
2018-04-18 14:24:34 -0400

    Add test arduino scripts

---
+++ test/servos_test/servos_test.ino
@@ -0,0 +1,54 @@
+#include <Servo.h>
+
+Servo pan, tilt, neck, ears, tail;
+
+void setup() {
+  Serial.begin(115200);
+
+  pan.attach(2);
+  tilt.attach(3);
+  neck.attach(4);
+  ears.attach(5);
+  tail.attach(6);
+
+  pan.write(90);
+  tilt.write(90);
+  neck.write(90);
+  ears.write(90);
+  tail.write(90);
+}
+
+#define TEST_SERVO(s, min_angle, max_angle) \
+    s.write(min_angle); \
+    delay(500); \
+    s.write(90); \
+    delay(500); \
+    s.write(max_angle); \
+    delay(500); \
+    s.write(90); \
+    delay(1000);
+
+void loop() {
+  // Test panning
+  TEST_SERVO(pan, 80, 100);
+
+  // Test tilting
+  TEST_SERVO(tilt, 75, 105);
+
+  // Test neck
+  TEST_SERVO(neck, 45, 135);
+
+  // Test ears
+  // TODO
+
+  // Test tail
+  // TODO
+
+  // Restore original positions
+  pan.write(90);
+  tilt.write(90);
+  neck.write(90);
+  ears.write(90);
+  tail.write(90);
+  delay(5000);
+}
