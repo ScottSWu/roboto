bff252d0c03360713443f234bd6bec973c8a962d
Scott Wu <scott.wu.hjx@gmail.com>
2018-05-08 00:06:00 -0400

    Add arduino code for mini Roboto

---
+++ romini/romini.ino
@@ -0,0 +1,29 @@
+#include <Servo.h>
+
+Servo s;
+
+void setup() {
+  Serial.begin(9600);
+  s.attach(2);
+  s.write(90);
+}
+
+#define PAUSE() delay(500);
+void loop() {
+  s.write(45);
+  PAUSE();
+  s.write(90);
+  PAUSE();
+  s.write(45);
+  PAUSE();
+  s.write(90);
+  PAUSE();
+  s.write(135);
+  PAUSE();
+  s.write(90);
+  PAUSE();
+  s.write(135);
+  PAUSE();
+  s.write(90);
+  PAUSE();
+}
