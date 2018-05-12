670b5ed900c884a5e870139fb3d85ca2966ba408
Scott Wu <scott.wu.hjx@gmail.com>
2018-05-12 12:15:24 -0400

    Update roduino code to assembly

--- roduino/roduino.ino
+++ roduino/roduino.ino
@@ -1,29 +1,42 @@
 #include <Servo.h>
 #include <LedControl.h>
 
-Servo pan, tilt, neck, ears, tail;
+Servo pan, tilt, neck, tail, earL, earR;
 LedControl eyes = LedControl(12, 11, 10, 2);
 
-void setup() {
-  Serial.begin(115200);
+#define SERIALIO Serial
 
+byte flip[256];
+void setup() {
+  SERIALIO.begin(115200);
+
+  // Generate flipped bytes
+  for (int i = 0; i < 256; i++) {
+    flip[i] = ((i & 1) << 7) | ((i & 2) << 5) | ((i & 4) << 3) | ((i & 8) << 1) |
+              ((i & 16) >> 1) | ((i & 32) >> 3) | ((i & 64) >> 5) | ((i & 128) >> 7);
+  }
+
+  pinMode(A0, INPUT);
+  
   pan.attach(2);
   tilt.attach(3);
   neck.attach(4);
-  ears.attach(5);
-  tail.attach(6);
+  tail.attach(5);
+  earL.attach(7);
+  earR.attach(6);
 
-  pan.write(95);
+  pan.write(90);
   tilt.write(90);
   neck.write(90);
-  ears.write(90);
   tail.write(90);
+  earL.write(90);
+  earR.write(90);
 
   eyes.shutdown(0, false);
   eyes.shutdown(1, false);
 
-  eyes.setIntensity(0, 15);
-  eyes.setIntensity(1, 15);
+  eyes.setIntensity(0, 1);
+  eyes.setIntensity(1, 1);
 
   eyes.clearDisplay(0);
   eyes.clearDisplay(1);
@@ -31,20 +44,27 @@ void setup() {
   setEyes(0);
 }
 
+#define PAN_CENTER 235
+
 char buffer[5];
 int buffer_index = 0;
+int pan_target = PAN_CENTER;
+long avg_feedback = ((long) pan_target * 1100) / 360;
 void loop() {
-  if (Serial.available() > 0) {
-    byte b = Serial.read();
+  if (SERIALIO.available() > 0) {
+    byte b = SERIALIO.read();
     buffer[buffer_index] = b;
     buffer_index++;
     if (buffer_index == 5 && b == '\n') {
       char t = buffer[0];
       int v = (buffer[1] - '0') * 100 + (buffer[2] - '0') * 10 + (buffer[3] - '0');
       switch (t) {
+        case ' ': // Ping
+          SERIALIO.println("Roboto!");
+          break;
         case 'p': // Pan
-          if (45 <= v && v <= 135) {
-            pan.write(v);
+          if (PAN_CENTER - 30 <= v && v <= PAN_CENTER + 30) {
+            pan_target = v;
           }
           break;
         case 't': // Tilt
@@ -57,6 +77,21 @@ void loop() {
             neck.write(v);
           }
           break;
+        case 'a': // Tail
+          if (75 <= v && v <= 105) {
+            tail.write(v);
+          }
+          break;
+        case 'l': // Left Ear
+          if (75 <= v <= 105) {
+            earL.write(v);
+          }
+          break;
+        case 'r': // Right Ear
+          if (75 <= v <= 105) {
+            earR.write(v);
+          }
+          break;
         case 'e': // Eyes
           setEyes(v);
           break;
@@ -65,6 +100,27 @@ void loop() {
     }
     else if (b == '\n' || buffer_index == 5) {
       buffer_index = 0;
+    }
+  }
+  long feedback = pulseIn(A0, HIGH, 2000);
+  if (feedback > 30) {
+    avg_feedback = (avg_feedback + feedback) / 2;
+    long delta = avg_feedback - ((long) pan_target * 1100) / 360;
+    /*
+    SERIALIO.print(avg_feedback);
+    SERIALIO.print(" ");
+    SERIALIO.print(feedback);
+    SERIALIO.print(" ");
+    SERIALIO.print(pan_target);
+    SERIALIO.print(" ");
+    SERIALIO.print(delta);
+    SERIALIO.print("\n");
+    */
+    if (delta == 0) {
+      pan.write(90);
+    }
+    else {
+      pan.write(90 + constrain(delta, -15, 15));
     }
   }
 }
@@ -110,12 +166,12 @@ byte sad[2][8] = {
 byte sleepy[2][8] = {
   { B00011000, B00011000, B00011001, B00011001, B00011001, B00011001, B00011000, B00011000 },
   { B00011000, B00011000, B00011001, B00011001, B00011001, B00011001, B00011000, B00011000 }
-};
+};  
 
 #define SHOW_EYES(mat) \
     for (int i = 0; i < 8; i++) { \
-      eyes.setRow(0, i, mat[0][i]); \
-      eyes.setRow(1, i, mat[1][i]); \
+      eyes.setColumn(0, i, flip[mat[1][i]]); \
+      eyes.setColumn(1, i, flip[mat[0][i]]); \
     } \
     break;
 
@@ -143,3 +199,5 @@ void setEyes(int t) {
       break;
   }
 }
+
+
